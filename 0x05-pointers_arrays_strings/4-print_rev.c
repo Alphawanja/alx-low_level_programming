12e790ffe019080d01bcfdc4b0d4fc0247662b22
Alphawanja <alphamariegarze823@gmail.com>
2022-10-25 12:15:48 -0700

    task 8

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
+ * print_rev - function that prints a string in reverse
+ * fcounter is to first count to end, n is to count back
  * @s: string to be printed
+ * Return: string in reverse
  */
 void print_rev(char *s)
 {
-	int i = 0;
+	int fcounter = 0;
+	int i, n;
 
-	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
 
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
  * @str: input
+ * Return: half of input
  */
 void puts_half(char *str)
 {
-	int len = 0;
+	int i, n, len;
 
-	while (*str != '\0')
-	{
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
 		len++;
-		str++;
-	}
 
-	str -= (len / 2);
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
