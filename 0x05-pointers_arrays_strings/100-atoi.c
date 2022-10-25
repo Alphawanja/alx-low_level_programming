7e3eeaf4d81a32ce5e5af3cd2e8edd27b2ccbef1
Alphawanja <alphamariegarze823@gmail.com>
2022-10-25 13:05:33 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	while (!('0' <= *s && *s <= '9') && *s != '0')
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '-')
 			sign *= -1;
@@ -23,6 +23,6 @@ int _atoi(char *s)
 		num = (num * 10) + (*s - '0');
 		s++;
 	}
-	return (num + sign);
+	return (num * sign);
 
 }
