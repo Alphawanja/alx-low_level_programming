77ccdca4e90a977e1db52ffd7ef9ac45c024d103
Alphawanja <alphamariegarze823@gmail.com>
2022-11-01 01:58:59 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates character in a string
+ * @s: is a pointer that stores the addresses
+ * @c: constant variable
+ * Return: (s + i)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (NULL);
+}
