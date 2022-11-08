e682d266291164ede202f13c65eb74f35a59200c
Alphawanja <alphamariegarze823@gmail.com>
2022-11-08 12:10:01 -0800

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concentrates all arguments of the program
+ * @ac: argument count
+ * @av: pointer to array of size ac
+ *
+ * Return: NULL if ac == 0 or av == NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, size;
+	char *arg;
+
+	size = 0;
+	k = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			size++;
+			i++;
+		}
+	}
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		arg[k] = av[i][j];
+		j++;
+		k++;
+	}
+	arg[k] = '\0';
+	return (arg);
+}
