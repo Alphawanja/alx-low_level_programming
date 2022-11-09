f9b7d9acf66e42e0a1c117ff31da93665c959fd1
Alphawanja <alphamariegarze823@gmail.com>
2022-11-09 06:59:07 -0800

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,16 @@ char *str_concat(char *s1, char *s2)
 	ptr = s1;
 	if (s1)
 		while (*ptr++)
+			size1++;
+	else
+		s1 = "";
+
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
 			size2++;
 	else
-		s2 = " ";
+		s2 = "";
 
 	ret = malloc(size1 + size2 + 1);
 	if (!ret)
