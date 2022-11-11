75c0e8f9626099f3d275fb96e60ce5d35de1fc6f
Alphawanja <alphamariegarze823@gmail.com>
2022-11-11 12:06:52 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -35,7 +35,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (0);
 
-	p = malloc(nmemb * nmemb);
+	p = malloc(nmemb * size);
 	if (p == 0)
 		return (0);
 	_memset(p, 0, size * nmemb);
