c236d07785c4465ed261fbc449634cd3eeb92e27
Alphawanja <alphamariegarze823@gmail.com>
2022-11-15 03:22:03 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog
+ * @d: the dog to be frred
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
