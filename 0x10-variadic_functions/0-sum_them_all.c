4a8fa0ba6151a1cff92735bb68465efceec69f11
Alphawanja <alphamariegarze823@gmail.com>
2022-11-18 04:14:08 -0800

    task 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
  * sum_them_all - adds num
  * @n: parameter
  *
- * Return: int
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -12,14 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if (n == 0)
-		return (0);
 	 va_start(valist, n);
-
 	 for (i = 0; i < n; i++)
 		 sum += va_arg(valist, int);
-
 	 va_end(valist);
-
 	 return (sum);
 }
