30cf7c8c9850591bef2da8d1a03e1874cddc82d3
Alphawanja <alphamariegarze823@gmail.com>
2022-11-30 11:15:03 -0800

    tasks

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list
+ * @head: head of a lis
+ * @index: index of the list where the node is
+ * deleted
+ *
+ * Return: 1 on success else -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	uns
