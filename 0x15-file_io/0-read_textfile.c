e6179bd595121222cd11ab445313d89bd1383d33
Alphawanja <alphamariegarze823@gmail.com>
2022-12-07 10:52:51 -0800

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * @filename: filename
  * @letters: numbers of letters printed
  *
- * Return: numbers of letters printed if fails, return 0
+ * Return: numbers of letters it can read and print, otherwise 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -32,4 +32,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buf);
 
 	return (nwr);
-
+}
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, rwr, nl;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nl = 0; text_content[nl]; nl++);
+	for (nl = 0; text_content[nl]; nl++)
 
 	rwr = write(fd, text_content, nl);
 
