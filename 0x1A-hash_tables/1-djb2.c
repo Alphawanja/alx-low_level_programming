e24409fcdb2e64d4409650eac46c1770a300737d
Alphawanja <alphamariegarze823@gmail.com>
2023-02-03 04:09:09 -0800

    hash tables

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-djb2.c
+ */
+
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: the string to hash
+ *
+ * Return: the calculated hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
